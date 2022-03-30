f0bb1b4a412f2997290a3607d39dab03dcad1004
何渊 <hzheyuan@corp.netease.com>
2022-03-30 20:39:51 +0800

    style(test): update test for none_modifying
    update test for none_modifying

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,6 +1,8 @@
-import { Vector } from '../src/';
-
-describe('add', () => {
-  it('should add two numbers', () => {
-  });
-});
+/*
+ * @Author: hzheyuan
+ * @Date: 2022-03-17 19:38:34
+ * @LastEditTime: 2022-03-30 20:38:50
+ * @LastEditors: hzheyuan
+ * @Description: 
+ * @FilePath: /tstl/__test__/index.spec.ts
+ */
